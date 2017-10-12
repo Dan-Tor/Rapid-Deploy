82c2d6f072e27c367dbd59d795b4084d1ab7f375
Dan.Tor <danielx0@hotmail.com>
2017-10-12 16:59:18 -0500

    Let's see if this one does it
    still not connecting to heroku

--- server.js
+++ server.js
@@ -90,8 +90,8 @@ if (app.get('env') === 'production') {
   });
 }
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+server.listen(config.port, function () {
+  console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
 });
 
 module.exports = app;
